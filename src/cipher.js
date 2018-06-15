9953fae50dcb1e5c527616acf3c8ee6243b9ab69
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-06-15 02:13:39 -0500

    Version con tests

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,37 @@
-//window.cipher = {
+window.cipher = {
 
 
   //Para cifrar el mensaje
-  const cifrarMensaje=()=> {
-    //Captar mensaje
-    let mensaje = document.getElementById('myMessage').value;
-    //Captar n, numero fijo para cifrar
-    let offset = document.getElementById('offSet').value;
-    //Variables para llevar a cabo el cifrado
+//  key:
+  encode:(string,key)=> {
+
+  //Variables para llevar a cabo el cifrado
     let cifrado="";
     let cifradoCesar = 0;
 
   //For para recorrer el mensaje captado
-      for(let i=0; i<mensaje.length; i++){
+      for(let i=0; i<string.length; i++){
 
     //Obtener el número del codigo ASCII de cada letra cifrada
      // Para mayusculas
-        if(mensaje.charCodeAt(i) >= 65 && mensaje.charCodeAt(i) <= 90 ) {
-          cifradoCesar = (mensaje.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+        if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90 ) {
+          cifradoCesar = (string.charCodeAt(i) - 65 + parseInt(key)) % 26 + 65;
         }
         // Para minusculas
-        else if(mensaje.charCodeAt(i) >= 97 && mensaje.charCodeAt(i) <= 122){
-          cifradoCesar=(mensaje.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+        else if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
+          cifradoCesar=(string.charCodeAt(i) - 97 + parseInt(key)) % 26 + 97;
         }
         // Para el espacio
-       else if(mensaje.charCodeAt(i) == 32) {
-          cifradoCesar=(mensaje.charCodeAt(i));
+       else if(string.charCodeAt(i) == 32) {
+          cifradoCesar=(string.charCodeAt(i));
         }
         //Para los caracteres ASCII 33 al 64
-        else if(mensaje.charCodeAt(i) >= 33 && mensaje.charCodeAt(i) <= 64){
-          cifradoCesar = (mensaje.charCodeAt(i));
+        else if(string.charCodeAt(i) >= 33 && string.charCodeAt(i) <= 64){
+          cifradoCesar = (string.charCodeAt(i));
         }
         //Para los caracteres ASCII 123 al 126
-        else if(mensaje.charCodeAt(i) >= 123 && mensaje.charCodeAt(i) <= 170){
-          cifradoCesar = (mensaje.charCodeAt(i));
+        else if(string.charCodeAt(i) >= 123 && string.charCodeAt(i) <= 170){
+          cifradoCesar = (string.charCodeAt(i));
         }
 
         // Obtener la letra cifrada
@@ -41,59 +39,63 @@
     //Acumular las letras cifradas, para formar la palabra cifrada
         cifrado+=mensajeCifrado;
       }
-    //Mostrar el mensaje cifrado en el output
-      return document.getElementById('myMessage2').value = cifrado;
-  }
+    //Retornar el mensaje cifrado
+      return cifrado;
+  },
+
+
+
+
 
 
   //Para descifrar el mensaje
-  const descifrarMensaje=()=> {
-    //Captar mensaje
-    let myMessage2 = document.getElementById('myMessage2').value;
-    //Captar n, numero fijo para cifrar
-    let offset = document.getElementById('offSet').value;
+decode: (string2,key)=> {
+
     //Variables para llevar a cabo el cifrado
     let descifrado="";
     let cifradoCesar = 0;
 
 
-      for(let i=0; i<myMessage2.length; i++){
+    for(let i=0; i<string2.length; i++){
 
       //Obtener el número del codigo ASCII de cada letra descifrada
-        if(myMessage2.charCodeAt(i) >= 65 && myMessage2.charCodeAt(i) <= 90){
-          cifradoCesar=(myMessage2.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+        //Para las mayusculas
+    if(string2.charCodeAt(i) >= 65 && string2.charCodeAt(i) <= 90){
 
+        cifradoCesar=(string2.charCodeAt(i) + 65 - parseInt(key)) % 26 + 65;
 
-        } else if (myMessage2.charCodeAt(i) >= 97 && myMessage2.charCodeAt(i) <= 122){
-          cifradoCesar=(myMessage2.charCodeAt(i) +97 + parseInt(offset)) % 26 + 97;
-        }
+        //Para las minusculas
+
+      } else if (string2.charCodeAt(i) >= 97 && string2.charCodeAt(i) <= 122){
+
+        cifradoCesar=(string2.charCodeAt(i) +97 + parseInt(key)) % 26 + 97;
+      }
 
         // Para el espacio
-        else if(myMessage2.charCodeAt(i) == 32) {
-          cifradoCesar=(myMessage2.charCodeAt(i));
-        }
+      else if(string2.charCodeAt(i) == 32) {
+        cifradoCesar=(string2.charCodeAt(i));
+      }
 
         //Para los caracteres ASCII 33 al 64
-        else if(myMessage2.charCodeAt(i) >= 33 && myMessage2.charCodeAt(i) <= 64){
-          cifradoCesar = (myMessage2.charCodeAt(i));
-        }
+      else if(string2.charCodeAt(i) >= 33 && string2.charCodeAt(i) <= 64){
+        cifradoCesar = (string2.charCodeAt(i));
+      }
 
         //Para los caracteres ASCII 123 al 126
-        else if(myMessage2.charCodeAt(i) >= 123 && myMessage2.charCodeAt(i) <= 170){
-          cifradoCesar = (myMessage2.charCodeAt(i));
-        }
-
-
-    // Obtener la letra descifrada
-        let mensajeDescifrado= String.fromCharCode(cifradoCesar);
-    //Acumular las letras descifradas
-        descifrado+=mensajeDescifrado;
-        console.log(descifrado);
+      else if(string2.charCodeAt(i) >= 123 && string2.charCodeAt(i) <= 170){
+        cifradoCesar = (string2.charCodeAt(i));
       }
-      return document.getElementById('myMessage').value = descifrado;
-  }
+
+        // Obtener la letra descifrada
+      let mensajeDescifrado= String.fromCharCode(cifradoCesar);
+
+        //Acumular las letras descifradas
+      descifrado+=mensajeDescifrado;
+
+      }
+        //Retornar mensaje descifrado
+        return descifrado;
+      }
 
 
-
-
-//};
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 
+//Captar mensaje
+  let mensaje = document.getElementById('myMessage');
+//Captar n, numero fijo para cifrar
+  let offset = document.getElementById('offSet');
+
+
+const cifrarMensaje=()=> {
+  let string = mensaje.value;
+  let key = offset.value;
+
+  //Pintar el mensaje cifrado en
+document.getElementById('myMessage2').value = cipher.encode(string,key);
+  //console.log(cipher.encode(string,key));
+  };
+
+
+
+// Para el descifrado
+  //Captamos mensaje a descifrar
+  let myMessage2 = document.getElementById('myMessage2');
+    //Captar n, numero fijo para cifrar
+//let offset = document.getElementById('offSet').value;
+
+//Llamar al evento de onclick
+  const descifrarMensaje=()=> {
+    let string2 = myMessage2.value;
+    let key = offset.value;
+
+// Pintar el mensaje descifrado
+
+  document.getElementById('myMessage').value = cipher.decode(string2,key);
+ };
